52071f116a102bee9f08535b49e8cc09be6fddca
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2015-12-09 21:13:48 -0500

    More linear brightness change

--- arduino/blah/blah.ino
+++ arduino/blah/blah.ino
@@ -2,16 +2,18 @@ const int gatePin = 5;
 
 void setup() {
   pinMode(gatePin, OUTPUT);
-  analogWrite(gatePin, 0);
+  Serial.begin(9600);
 }
 
 void loop() {
-  for (int i = 1; i <= 1023; i++) {
-    analogWrite(gatePin, i);
-    delay(5);
+  for (int i = 1; i <= 255; i++) {
+    float j = (float) i;
+    analogWrite(gatePin, (int)(0.003906 * j * j));
+    delay(20);
   }
-  for (int i = 1022; i >= 0; i--) {
-    analogWrite(gatePin, i);
-    delay(5);
+  for (int i = 255; i >= 0; i--) {
+    float j = (float) i;
+    analogWrite(gatePin, (int)(0.003906 * j * j));
+    delay(20);
   }
 }
