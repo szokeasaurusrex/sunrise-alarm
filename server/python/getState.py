9527afafb755f3424dfcc6439d9d86ec334d9eac
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-01-07 19:22:59 -0500

    No blank line in output

--- server/python/getState.py
+++ server/python/getState.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python
 import serial
 import time
+import sys
 port = serial.Serial('/dev/ttyACM0', 9600)
 port.write("i")
 time.sleep(0.1)
 light_state = port.readline()
-print light_state
+sys.stdout.write(light_state)
