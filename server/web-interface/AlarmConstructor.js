3e6d2267eac636e33ca27e97609e457ba0f2ee59
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-01-22 12:31:52 -0500

    Checks for valid hour and minute

--- server/web-interface/AlarmConstructor.js
+++ server/web-interface/AlarmConstructor.js
@@ -58,16 +58,26 @@ function Alarm(hour, minute, days, active) {
     console.log(this);
   };
   this.saveEdits = function() {
-    this.editing = false;
+    this.hour = parseInt(this.hour);
+    this.minute = parseInt(this.minute);
     this.updateDays();
-    this.updateStartTime();
-    console.log(this)
-    $.post("saveAlarm.php", {type: "edit", alarm: JSON.stringify(this)}, function (response) {
-      if (response == "OK") {
-        alert("The alarm was successfully updated.");
-      } else {
-        alert("FAIL " + response);
-      }
-    });
+    if (this.hour < 0 || this.hour > 23) {
+      alert("Error. The alarm hour must be a number between 0 and 23.");
+    } else if (this.minute < 0 || this.minute > 59) {
+      alert("Error. The alarm minute must be a number between 0 and 59.");
+    } else if (this.days == []) {
+      alert("Error. No days were selected.");
+    } else {
+      this.editing = false;
+      this.updateStartTime();
+      console.log(this)
+      $.post("saveAlarm.php", {type: "edit", alarm: JSON.stringify(this)}, function (response) {
+        if (response == "OK") {
+          alert("The alarm was successfully updated.");
+        } else {
+          alert("FAIL " + response);
+        }
+      });
+    }
   }
 }).call(Alarm.prototype);
