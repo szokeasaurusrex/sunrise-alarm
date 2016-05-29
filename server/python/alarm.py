f102d5081355c3767822dec18c38be3bf38bc9d0
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-05-28 23:01:53 -0400

    IFTTT notify of alarm

--- server/python/alarm.py
+++ server/python/alarm.py
@@ -1,4 +1,13 @@
 #! /usr/bin/python3
 import serial
-port = serial.Serial('/dev/ttyACM0', 9600)
-port.write('a')
+import os
+import requests
+
+# start alarm
+# port = serial.Serial('/dev/ttyACM0', 9600)
+# port.write('a')
+
+#ifttt
+ifttt_key = os.environ['IFTTT_KEY']
+url = 'https://maker.ifttt.com/trigger/sunalarm/with/key/' + ifttt_key
+requests.post(url)
