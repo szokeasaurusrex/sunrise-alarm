0892396ee51aefc4d1570599c7cf16175b1e0fc2
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-09-30 23:35:59 -0400

    Fade in and fade out, dim functioni

--- arduino/alarm/alarm.ino
+++ arduino/alarm/alarm.ino
@@ -4,24 +4,32 @@ String light_state;
 void serialEvent();
 void alarm();
 
-void alarm() {
-  boolean alarm_stopped = false;
+void dim(unsigned long duration, boolean brighten) {
+  double lnb = log(pow(256.0, 1.0 / (float) duration));
   for (int i = 1; i < 256; i++) {
-    if (light_state != "alarm") {
-      boolean alarm_stopped = true;
+    if (light_state != "dimming") {
       return;
     }
-    int brightness = (int) pow(1.0219, (float) i);
-    analogWrite(gate_pin, brightness);
-    for (int j = 0; j < 7059; j++) {
-      if (Serial.available() > 0) {
-        serialEvent();
+    unsigned long interval;
+    if (brighten) {
+      analogWrite(gate_pin, i);
+      interval = (unsigned long) round(log(((float) i + 1) / (float) i) / lnb);
+    } else {
+      int x = 256 - i;
+      analogWrite(gate_pin, x - 1);
+      interval = (unsigned long) round(log((float) x / ((float) x - 1)) / lnb);
+    }
+    unsigned long previousMillis = millis();
+    if (i < 255) {
+      while(millis() - previousMillis < interval) {
+        loop();
       }
-      delay(1);
     }
   }
-  if (!(alarm_stopped)) {
+  if (brighten) {
     light_state = "on";
+  } else {
+    light_state = "off";
   }
 }
 
@@ -31,20 +39,17 @@ void serialEvent() {
   if (serial_data == "i") {
     Serial.println(light_state);
   } else if (light_state != "on" && serial_data == "o") {
-    analogWrite(gate_pin, 255);
-    light_state = "on";
+    light_state = "dimming";
+    dim(500, true);
     Serial.println(light_state);
   } else if (serial_data == "o") {
-    analogWrite(gate_pin, 0);
-    light_state = "off";
+    light_state = "dimming";
+    dim(500, false);
     Serial.println(light_state);
   } else if (serial_data == "a" && light_state == "off") {
-    light_state = "alarm";
-    alarm();
-  } //else {
-  //   return false;
-  // }
-  // return true;
+    light_state = "dimming";
+    dim(1800000, true);
+  }
 }
 
 void setup() {
