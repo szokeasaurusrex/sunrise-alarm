f82ae589ba5bda164d8b654c050a74ea1bd9dd14
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-10-07 21:10:50 -0400

    reverted to sys.stdout.write

--- server/python/toggle.py
+++ server/python/toggle.py
@@ -6,4 +6,4 @@ import sys
 port = serial.Serial('/dev/ttyACM0', 9600)
 port.write(b"o")
 light_state = port.readline()
-print(light_state.decode("utf-8"))
+sys.stdout.write(light_state.decode("utf-8"))
