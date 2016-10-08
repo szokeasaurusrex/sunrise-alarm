260e35fdbd85d703d27a9beb3bbeca8acc01ba93
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-10-07 21:00:37 -0400

    cleaned up python files

--- server/python/alarm.py
+++ server/python/alarm.py
@@ -3,11 +3,6 @@ import serial
 import os
 import requests
 
-# start alarm
-# port = serial.Serial('/dev/ttyACM0', 9600)
-# port.write('a')
-
-#ifttt
-ifttt_key = os.environ['IFTTT_KEY']
-url = 'https://maker.ifttt.com/trigger/sunalarm/with/key/' + ifttt_key
-requests.post(url)
+start alarm
+port = serial.Serial('/dev/ttyACM0', 9600)
+port.write('a')
--- server/python/getState.py
+++ server/python/getState.py
@@ -1,9 +1,7 @@
 #! /usr/bin/python3
 import serial
-#import time
 import sys
 port = serial.Serial('/dev/ttyACM0', 9600)
 port.write("i")
-#time.sleep(0.1)
 light_state = port.readline()
 sys.stdout.write(light_state)
