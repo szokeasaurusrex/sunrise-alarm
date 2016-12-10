3c27978e43d6855578d27884c00d1e9fffb823c2
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-12-09 22:04:44 -0500

    Added better error/response detection

--- server/web-interface/admin/admin.js
+++ server/web-interface/admin/admin.js
@@ -1,33 +1,46 @@
 app.controller("admin", function($scope) {
   function main() {
     if (localStorage.authkey) {
-      var authkey = JSON.stringify(localStorage.authkey);
+      var authkey = JSON.parse(localStorage.authkey);
     } else {
       var name = "";
       while (name == "") {
         name = prompt("Please name this new device");
+      }
+      while (name.length > 50) {
+        name = prompt("Error: Name must be 50 characters or less. Please enter a different name");
       }
       var authkey = {
         device: name,
         key: ""
       };
     }
+    $scope.authorized = false;
     $scope.device_name = name;
 
-    $.getJSON("getDevices.php", authkey, function(device_info) {
-      if (device_info == "unauthorized") {
-        $scope.authorized = false;
-      } else if (device_info == "nodevice") {
-        localStorage.removeItem("authkey");
-        main();
-      } else {
+// response object contains msg and device_info
+// device_info is array if msg "OK", if "newdevice" it is an object for one device
+    $.getJSON("getDevices.php", authkey, function(response) {
+      if (response.msg == "OK") {
         $scope.authorized = true;
         $scope.devices = [];
-        for (var i = 0; i < device_info.length; i++) {
-          $scope.devices[i] = new Device(device_info[i].name, device_info[i].authorized, device_info[i].current);
+        for (var i = 0; i < response.device_info.length; i++) {
+          $scope.devices[i] = new Device(response.device_info[i].name, response.device_info[i].authorized, response.device_info[i].current);
         }
-        $scope.$apply();
+      } else if (response.msg == "nodevice") {
+        alert("This device no longer exists in the database.");
+        localStorage.removeItem("authkey");
+        main();
+      } else if (response.msg == "newdevice") {
+        $scope.authorized = false;
+        localStorage.authkey = JSON.stringify(response.device_info)
+      } else if (response.msg == "duplicatename") {
+        alert("Sorry. The device name you picked exists in the database already. Pick another one.");
+        main()
+      } else {
+        alert(response.msg);
       }
+      $scope.$apply();
     });
   }
 });
