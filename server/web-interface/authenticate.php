056565152536687401400d96dde637bbd2330d11
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-12-10 09:16:28 -0500

    Make sure that there is only 1 such device

--- server/web-interface/authenticate.php
+++ server/web-interface/authenticate.php
@@ -3,9 +3,14 @@
     $sql = $conn->prepare("SELECT * FROM devices WHERE name = ?");
     $sql = $conn->bind_param("s", $device_name);
     $sql->execute();
-    $device = $sql->get_result()->fetch_assoc();
-    if ($device["approved"] && password_verify($password, $device["hash"])) {
-      return true;
+    $result = $sql->get_result();
+    if ($result->num_rows == 1) {
+      $device = $result->fetch_assoc();
+      if ($device["approved"] && password_verify($password, $device["hash"])) {
+        return true;
+      } else {
+        return false;
+      }
     } else {
       return false;
     }
