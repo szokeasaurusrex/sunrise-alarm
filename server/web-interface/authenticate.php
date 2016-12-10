c6de7b43abd251e2f4ec316fb8a78e849089304a
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-12-09 22:25:48 -0500

    Security fix

--- server/web-interface/authenticate.php
+++ server/web-interface/authenticate.php
@@ -1,8 +1,10 @@
 <?php
   function authenticate($conn, $device_name, $password) {
-    $sql = "SELECT * FROM devices WHERE name=$device_name";
-    $device = $conn->query($sql)->fetch_assoc();
-    if ($device.approved && password_verify($password, $device.hash)) {
+    $sql = $conn->prepare("SELECT * FROM devices WHERE name = ?");
+    $sql = $conn->bind_param("s", $device_name);
+    $sql->execute();
+    $device = $sql->get_result()->fetch_assoc();
+    if ($device["approved"] && password_verify($password, $device["hash"])) {
       return true;
     } else {
       return false;
