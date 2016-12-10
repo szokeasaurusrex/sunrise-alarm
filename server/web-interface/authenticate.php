c7b442811ba9d2d17f2e6b3148913782f4705c20
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-12-10 09:22:31 -0500

    Fix bind_param

--- server/web-interface/authenticate.php
+++ server/web-interface/authenticate.php
@@ -1,7 +1,7 @@
 <?php
   function authenticate($conn, $device_name, $password) {
     $sql = $conn->prepare("SELECT * FROM devices WHERE name = ?");
-    $sql = $conn->bind_param("s", $device_name);
+    $sql->bind_param("s", $device_name);
     $sql->execute();
     $result = $sql->get_result();
     if ($result->num_rows == 1) {
