d4208df3988495efe38fed5df2aaec6de5e9cc66
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2016-12-10 10:51:54 -0500

    convert authorized from int to boolean

--- server/web-interface/admin/DeviceConstructor.js
+++ server/web-interface/admin/DeviceConstructor.js
@@ -1,6 +1,6 @@
 function Device(name, authorized, current_name) {
   this.name = name;
-  this.authorized = authorized;
+  this.authorized = (authorized == 1) ? true : false;
   this.current = (current_name == name);
 }
 // TODO add functions
